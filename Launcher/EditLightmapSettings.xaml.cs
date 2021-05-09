e0cc034d7b6df4a41b5b53fe8ed81a5a6cec3145
Berthalamew <none>
2021-05-08 23:43:39 -0400

    Fixups for previous commit

--- Launcher/EditLightmapSettings.xaml.cs
+++ Launcher/EditLightmapSettings.xaml.cs
@@ -1,62 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.IO;
-using ToolkitLauncher.Properties;
 using ToolkitLauncher.ToolkitInterface;
-
 
 namespace ToolkitLauncher
 {
-
     /// <summary>
-    /// Interaction logic for EditLightmapSettings.xaml
+    ///     Interaction logic for EditLightmapSettings.xaml
     /// </summary>
-    public partial class EditLightmapSettings : Window
+    public partial class EditLightmapSettings
     {
-        ToolkitBase[] toolkits = new ToolkitBase[]
-        {
-            new H1Toolkit(),
-            new H2Toolkit()
-        };
-        ToolkitBase toolkit
-        {
-            get
-            {
-                return toolkits[1];
-            }
-        }
+        public string LightmapConfigFile => toolkit.GetBaseDirectory() + @"\custom_lightmap_quality.conf";
 
+        private static ToolkitBase toolkit => new H2Toolkit();
+
+        private string[] lines;
+        class DefaultLightmapConfigText
+        {
+            public string line0 = "is_checkboard = false";
+            public string line0_t = "is_checkboard = false";
+            public string line1 = "is_direct_only = false";
+            public string line1_t = "is_direct_only = true";
+            public string line2 = "is_draft = false";
+            public string line2_t = "is_draft = false";
+            public string line3 = "main_monte_carlo_setting = 8";
+            public string line4 = "proton_count = 20000000";
+            public string line5 = "secondary_monte_carlo_setting = 4";
+            public string line6 = "unk7 = 4";
+        }
         public EditLightmapSettings()
         {
-            string lightmap_config = (toolkit.GetBaseDirectory() + @"\custom_lightmap_quality.conf");
-            bool file_exists = File.Exists(lightmap_config);
-            string[] lines;
-            if (file_exists == false)
+            if (File.Exists(LightmapConfigFile) == false)
             {
-                using (StreamWriter sw = File.CreateText(lightmap_config))
-                {
-                    sw.WriteLine("is_checkboard = false");
-                    sw.WriteLine("is_direct_only = false");
-                    sw.WriteLine("is_draft = false");
-                    sw.WriteLine("main_monte_carlo_setting = 8");
-                    sw.WriteLine("proton_count = 20000000");
-                    sw.WriteLine("secondary_monte_carlo_setting = 4");
-                    sw.WriteLine("unk7 = 4");
-
-                }
+                SaveConfigFile();
             }
-            lines = File.ReadAllLines(lightmap_config);
+
+            lines = File.ReadAllLines(LightmapConfigFile);
 
             InitializeComponent();
 
@@ -66,73 +44,65 @@ namespace ToolkitLauncher
             txt_gather_distance.Text = lines[6].Remove(0, 7);
 
             if (lines[0].Remove(0, 16).Contains("true"))
-            {
                 chk_is_checkboard.IsChecked = true;
-            }
             else
-            {
                 chk_is_checkboard.IsChecked = false;
-            }
+
             if (lines[1].Remove(0, 17).Contains("true"))
-            {
                 chk_is_direct_only.IsChecked = true;
-            }
             else
-            {
                 chk_is_direct_only.IsChecked = false;
-            }
+
             if (lines[2].Remove(0, 11).Contains("true"))
-            {
                 chk_is_draft.IsChecked = true;
-            }
             else
-            {
                 chk_is_draft.IsChecked = false;
+        }
+
+        public void SaveConfigFile()
+        {
+            DefaultLightmapConfigText lightmapConfigText = new DefaultLightmapConfigText();
+
+            using (StreamWriter sw = File.CreateText(LightmapConfigFile))
+            {
+                sw.WriteLine(lightmapConfigText.line0);
+                sw.WriteLine(lightmapConfigText.line1);
+                sw.WriteLine(lightmapConfigText.line2);
+                sw.WriteLine(lightmapConfigText.line3);
+                sw.WriteLine(lightmapConfigText.line4);
+                sw.WriteLine(lightmapConfigText.line5);
+                sw.WriteLine(lightmapConfigText.line6);
             }
         }
 
         private void btn_save_changes_Click(object sender, RoutedEventArgs e)
         {
-            string lightmap_config = (toolkit.GetBaseDirectory() + @"\custom_lightmap_quality.conf");
-            string[] lines = File.ReadAllLines(lightmap_config);
+            lines = File.ReadAllLines(LightmapConfigFile);
 
             //Saves the file with new settings based on what the user selected
             if (chk_is_checkboard.IsChecked == true)
-            {
                 lines[0] = "is_checkboard = true";
-            }
             else
-            {
                 lines[0] = "is_checkboard = false";
-            }
             if (chk_is_direct_only.IsChecked == true)
-            {
                 lines[1] = "is_direct_only = true";
-            }
             else
-            {
                 lines[1] = "is_direct_only = false";
-            }
             if (chk_is_draft.IsChecked == true)
-            {
                 lines[2] = "is_draft = true";
-            }
             else
-            {
                 lines[2] = "is_draft = false";
-            }
             lines[3] = "main_monte_carlo_setting = " + txt_sample_count.Text;
             lines[4] = "proton_count = " + txt_photon_count.Text;
             lines[5] = "secondary_monte_carlo_setting = " + txt_AA_sample_count.Text;
             lines[6] = "unk7 = " + txt_gather_distance.Text;
 
-            File.WriteAllLines(lightmap_config, lines);
-            this.Close();
+            File.WriteAllLines(LightmapConfigFile, lines);
+            Close();
         }
 
         private void btn_reset_changes_Click(object sender, RoutedEventArgs e)
         {
-
             txt_sample_count.Text = "8";
             txt_photon_count.Text = "20000000";
             txt_AA_sample_count.Text = "4";
--- Launcher/PathSettings.xaml.cs
+++ Launcher/PathSettings.xaml.cs
@@ -140,7 +140,12 @@ namespace ToolkitLauncher
             string root_directory_path = root_directory.Text;
             int profile_template_index = profile_type.SelectedIndex;
             profile_wizard_menu.Visibility = Visibility.Collapsed;
-            CreateProfileTemplate(root_directory_path, profile_template_index);
+
+            //Check to see if path for directory is empty to prevent crash in profile wizard
+            if (String.IsNullOrEmpty(root_directory_path) == false)
+            {
+                CreateProfileTemplate(root_directory_path, profile_template_index);
+            }
         }
 
         private void browse_root_directory_Click(object sender, RoutedEventArgs e)
@@ -313,70 +318,64 @@ namespace ToolkitLauncher
                     break;
             }
 
-            //Check to see if path for directory is empty to prevent crash in profile wizard
-            if (String.IsNullOrEmpty(root_directory_path))
-            {
-            }
-            else
-            {
-                string[] fileEntries = Directory.GetFiles(root_directory_path);
-                bool tool_hash_matched = false;
-                bool guerilla_hash_matched = false;
-                bool sapien_hash_matched = false;
-                foreach (string fileName in fileEntries)
-                    if (Path.GetExtension(fileName) == ".exe")
+            string[] fileEntries = Directory.GetFiles(root_directory_path);
+            bool tool_hash_matched = false;
+            bool guerilla_hash_matched = false;
+            bool sapien_hash_matched = false;
+            foreach (string fileName in fileEntries)
+                if (Path.GetExtension(fileName) == ".exe")
+                {
+                    var md5 = System.Security.Cryptography.MD5.Create();
+                    var stream = File.OpenRead(fileName);
+                    string hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "");
+
+                    if (!tool_hash_matched)
                     {
-                        var md5 = System.Security.Cryptography.MD5.Create();
-                        var stream = File.OpenRead(fileName);
-                        string hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "");
-
-                        if (!tool_hash_matched)
+                        foreach (string hek_tool_md5 in hek_tool_md5_array)
                         {
-                            foreach (string hek_tool_md5 in hek_tool_md5_array)
+                            if (hek_tool_md5.Contains(hash))
                             {
-                                if (hek_tool_md5.Contains(hash))
-                                {
-                                    hek_tool_path = fileName;
-                                    tool_hash_matched = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!guerilla_hash_matched)
-                        {
-                            foreach (string hek_guerilla_md5 in hek_guerilla_md5_array)
-                            {
-
-                                if (hek_guerilla_md5.Contains(hash))
-                                {
-                                    hek_guerilla_path = fileName;
-                                    guerilla_hash_matched = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!sapien_hash_matched)
-                        {
-                            foreach (string hek_sapien_md5 in hek_sapien_md5_array)
-                            {
-                                if (hek_sapien_md5.Contains(hash))
-                                {
-                                    hek_sapien_path = fileName;
-                                    sapien_hash_matched = true;
-                                    break;
-                                }
+                                hek_tool_path = fileName;
+                                tool_hash_matched = true;
+                                break;
                             }
                         }
                     }
+                    if (!guerilla_hash_matched)
+                    {
+                        foreach (string hek_guerilla_md5 in hek_guerilla_md5_array)
+                        {
 
-                profile_name.Text = profile_name_template;
-                tool_path.Text = hek_tool_path;
-                sapien_path.Text = hek_sapien_path;
-                guerilla_path.Text = hek_guerilla_path;
-                gen_type.SelectedIndex = gen_type_template;
-                build_type.SelectedIndex = build_type_template;
-                community_tools.IsChecked = community_tools_template;
-            }
+                            if (hek_guerilla_md5.Contains(hash))
+                            {
+                                hek_guerilla_path = fileName;
+                                guerilla_hash_matched = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!sapien_hash_matched)
+                    {
+                        foreach (string hek_sapien_md5 in hek_sapien_md5_array)
+                        {
+                            if (hek_sapien_md5.Contains(hash))
+                            {
+                                hek_sapien_path = fileName;
+                                sapien_hash_matched = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            profile_name.Text = profile_name_template;
+            tool_path.Text = hek_tool_path;
+            sapien_path.Text = hek_sapien_path;
+            guerilla_path.Text = hek_guerilla_path; 
+            gen_type.SelectedIndex = gen_type_template;
+            build_type.SelectedIndex = build_type_template;
+            community_tools.IsChecked = community_tools_template;
+
         }
     }
 
